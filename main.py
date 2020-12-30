a70098896ad3877d9b71894587377f5abc5e93e9
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-12-30 17:38:52 +0100

    auth

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from google.auth.transport import requests
 from google.cloud import datastore
+import google.oauth2.id_token
+
+app = Flask(__name__)
+firebase_request_adapter = requests.Request()
+
+@app.route('/')
+def root():
+    # Verify Firebase auth.
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+    times = None
+
+    if id_token:
+        try:
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+            claims = google.oauth2.id_token.verify_firebase_token(
+                id_token, firebase_request_adapter)
+        except ValueError as exc:
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc)
+
+        # Record and fetch the recent times a logged-in user has accessed
+        # the site. This is currently shared amongst all users, but will be
+        # individualized in a following step.
+        store_time(datetime.datetime.now())
+        times = fetch_times(10)
+
+    return render_template(
+        'index.html',
+        user_data=claims, error_message=error_message, times=times)
 
 datastore_client = datastore.Client()
 
@@ -21,21 +58,6 @@ def fetch_times(limit):
     times = query.fetch(limit=limit)
 
     return times
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def root():
-    # Store the current access time in Datastore.
-    store_time(datetime.datetime.now())
-
-    # Fetch the most recent 10 access times from Datastore.
-    times = fetch_times(10)
-
-    return render_template(
-        'index.html', times=times)
-
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
